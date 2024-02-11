7b1c3909b4952e939d5a0de85f7b05aec3fd8960
Diram Tabaa <dtabaa@andrew.cmu.edu>
2024-02-11 16:03:14 +0300

    first commit

---
+++ voronoi.c
@@ -0,0 +1,10 @@
+/**
+ * @file voronoi.c
+ * @author Diram Tabaa (dtabaa@andrew.cmu.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
---
+++ voronoi_main.c
@@ -0,0 +1,16 @@
+/**
+ * @file voronoi_main.c
+ * @author Diram Tabaa (dtabaa@andrew.cmu.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-11
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <stdio.h>
+#include "voronoi.h"
+
+int main(int argc, char** argv) {
+    return 0;
+}
